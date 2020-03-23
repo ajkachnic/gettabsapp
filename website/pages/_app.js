cea0088bf3d28f4e5e08c8b624ecdaa27f82ae2d
Andrew Kachnic <ajkachnic@protonmail.com>
2020-03-23 07:10:49 -0400

    Set Up SEO

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import App from 'next/app';
 import { NextSeo } from 'next-seo'
 import Head from 'next/head';
+import ReactGA from 'react-ga';
 
 export default class MyApp extends App {
   render() {
@@ -10,6 +11,7 @@ export default class MyApp extends App {
       <>
       <Head>
         <link rel="icon" href="/favicon.png" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" /> 
       </Head>
         <NextSeo
           title="Get Tabs App"
